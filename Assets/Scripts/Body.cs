2dfc934cafaaf5eaef6be4231065ced69dbb4629
Stephane Lin <lins.stephane@gmail.com>
2016-10-18 18:05:06 -0400

    Sample planet gravity

---
+++ Assets/Scripts/Body.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Body : MonoBehaviour {
+
+	public Attractor attractor;
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeRotation;
+		GetComponent<Rigidbody> ().useGravity = false;
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attractor.Attract (myTransform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2;
+	private Vector3 moveDirection;
+	
+	void Update () {
+		moveDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody> ().MovePosition (GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+	}
+}
