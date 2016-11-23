cbd5a9a1e8fb5565fc03a8df23bafbc2735abf9d
Jordan Orsini <jordan__o@hotmail.com>
2016-11-23 01:47:01 -0500

    boat puzzle

---
+++ Assets/Scripts/BoatScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatScript : MonoBehaviour 
+{
+    public GameObject otherBoat;
+
+    bool moveBoats;
+    bool stopBoats;
+
+    Transform otherBoatTransform;
+
+    void Start () 
+    {
+        otherBoatTransform = otherBoat.GetComponent<Transform>();
+        moveBoats = false;
+        stopBoats = false;
+	}
+	
+	void Update () 
+    {
+        if(moveBoats == true)
+        {
+            transform.Translate(Vector3.forward * 0.1f);
+            otherBoatTransform.Translate(Vector3.forward * 0.1f);
+
+        }
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Mage")
+        {
+            moveBoats = true;
+        }
+
+        if(col.gameObject.tag == "Wall")
+        {
+            moveBoats = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleController : MonoBehaviour 
+{
+    public GameObject cliff1;
+    public GameObject cliff2;
+    public GameObject cliff3;
+
+    Transform cliff1Transform;
+    Transform cliff2Transform;
+    Transform cliff3Transform;
+	
+    bool puzzle1Complete;
+    bool puzzle2Complete;
+
+    void Start () 
+    {
+        puzzle1Complete = true;
+        puzzle2Complete = true;
+        cliff1Transform = cliff1.GetComponent<Transform>();
+        cliff2Transform = cliff2.GetComponent<Transform>();
+        cliff3Transform = cliff3.GetComponent<Transform>();
+	}
+
+	void Update () 
+    {
+        if(puzzle1Complete == true && puzzle2Complete == true)
+        {
+            Destroy(cliff1);
+            Destroy(cliff2);
+            Destroy(cliff3);
+        }
+	}
+
+    public void setPuzzle1Complete()
+    {
+        puzzle1Complete = true;
+    }
+
+    public void setPuzzle2Complete()
+    {
+        puzzle2Complete = true;
+    }
+}
