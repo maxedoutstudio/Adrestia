1456d423c5b3de2c920a26e8d070a8a189ed1b65
Patrick <Patrick@Patricks-MacBook-Pro.local>
2016-11-26 15:57:02 -0500

    Added teleporter to fire planet

---
+++ Assets/FirePlanetTeleporter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FirePlanetTeleporter : MonoBehaviour {
+
+
+	public GameObject powerUpTracker;
+	PowerupTracker myPowerUpTrackerScript;
+
+	void Start()
+	{
+		myPowerUpTrackerScript = powerUpTracker.GetComponent<PowerupTracker>();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Player" && myPowerUpTrackerScript.getCanFire()) {
+			SceneManager.LoadScene ("Lightningworld");
+		}
+	}
+}
