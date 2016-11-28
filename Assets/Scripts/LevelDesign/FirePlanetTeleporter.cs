5c5431266a2cc27e86e333512bf36e3dd409c895
Patrick <Patrick@Patricks-MacBook-Pro.local>
2016-11-28 12:58:09 -0500

    Fixed teleporter appear and sound on fire planet to match with water planet. Didn't chenge pick up sound

--- Assets/Scripts/LevelDesign/FirePlanetTeleporter.cs
+++ Assets/Scripts/LevelDesign/FirePlanetTeleporter.cs
@@ -7,16 +7,29 @@ public class FirePlanetTeleporter : MonoBehaviour {
 
 	public GameObject powerUpTracker;
 	PowerupTracker myPowerUpTrackerScript;
+	public GameObject loadingScreen;
+	public AudioSource warpSound;
+	AudioSource myWarpSound;
 
 	void Start()
 	{
 		myPowerUpTrackerScript = powerUpTracker.GetComponent<PowerupTracker>();
+		myWarpSound = warpSound.GetComponent<AudioSource>();
+	}
+
+	void Update ()
+	{
+		if (myPowerUpTrackerScript.getCanFire ()) {
+			Instantiate(myWarpSound);
+			gameObject.GetComponent<ParticleSystem>().Play();
+		}
 	}
 
 	void OnTriggerEnter(Collider col)
 	{
 		if (col.gameObject.tag == "Player" && myPowerUpTrackerScript.getCanFire()) {
-			SceneManager.LoadScene ("Lightningworld");
+			loadingScreen.SetActive(true);
+			//SceneManager.LoadScene ("Lightningworld");
 		}
 	}
 }
