0c4ed0663d0a0266b9d68a066fd86ecab907ba66
Jordan Orsini <jordan__o@hotmail.com>
2016-11-28 15:04:36 -0500

    revamped main menu including chapter select

---
+++ Assets/Scripts/Menu/chapterSelectScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chapterSelectScript : MonoBehaviour 
+{
+    public GameObject menu1;
+    public GameObject menu2;
+
+    public void play()
+    {
+        menu1.SetActive(false);
+        menu2.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(menu2.activeSelf == true)
+            {
+                menu2.SetActive(false);
+                menu1.SetActive(true);
+            }
+        }
+    }
+}
